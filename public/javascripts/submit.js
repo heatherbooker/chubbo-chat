1ee595e22b63ddde8d4e24d69704593eaa005c08
Heather Booker <Heather Booker>
2016-07-05 19:43:22 -0700

    use ajax instead of res.render, add bootstrap

---
+++ public/javascripts/submit.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  $(document).keypress(function(e) {
+    if(e.which == 13) {
+      handleSubmit();
+    }
+  });
+
+  $('#submit').click(handleSubmit);
+
+});
+
+function handleSubmit() {
+  var message = $('#messageInput').val();
+  $('#messageInput').val('');
+
+  var settings = {
+    "url": "http://localhost:3000/reverse",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json"
+    },
+    "data": "{\"message\": \""+message+"\"}"
+  }
+
+  $.ajax(settings).done(function (response) {
+    var reversed = reverse(response);
+    $('.incoming').append(reversed + '<br />');
+    $('.outgoing').append(response + '<br />');
+  });
+}
+
+function reverse(message) {
+  var reverseMessage = '';
+  var messageMax = message.length;
+  for (var i = 0; i < messageMax; i++) {
+    reverseMessage = message.charAt(i) + reverseMessage;
+  }
+  return reverseMessage;
+}
--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'chubbo-chat',
-    incoming: 'none',
-    outgoing: 'none'
-  });
+  res.render('index', {title: 'chubbo-chat'});
 });
 
-
-function reverse(msg) {
-  var reverseMsg = '';
-  var msgMax = msg.length;
-  for (var i = 0; i < msgMax; i++) {
-    reverseMsg = msg.charAt(i) + reverseMsg;
-  }
-  return reverseMsg;
-}
-
-var incomingMsgs = [];
-var outgoingMsgs = [];
-
 // POST message text
-router.post('/', function(req, res) { 
-  var msg = req.body.msg;
-  outgoingMsgs.push(msg);
-  incomingMsgs.push(reverse(msg));
-  res.render('index', {
-    title: 'chubbo-chat',
-    incoming: incomingMsgs,
-    outgoing: outgoingMsgs
-  });
+router.post('/reverse', function(req, res) {
+  res.send(req.body.message);
 });
 
 module.exports = router;
