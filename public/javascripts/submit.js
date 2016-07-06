7a9d5061054fbdf0be18b766b295ce1096cc6d7e
Heather Booker <Heather Booker>
2016-07-05 23:23:34 -0700

    build landing page pug and css

--- public/javascripts/submit.js
+++ public/javascripts/submit.js
@@ -2,29 +2,7 @@ $(document).ready(function() {
 
   $(document).keypress(function(e) {
     if(e.which == 13) {
-      handleSubmit();
     }
   });
 
-  $('#submit').click(handleSubmit);
-
 });
-
-function handleSubmit() {
-  var message = $('#messageInput').val();
-  $('#messageInput').val('');
-
-  var settings = {
-    "url": "/reverse",
-    "method": "POST",
-    "headers": {
-      "content-type": "application/json"
-    },
-    "data": "{\"message\": \""+message+"\"}"
-  }
-
-  $.ajax(settings).done(function (response) {
-    $('.incoming').append(response + '<br />');
-    $('.outgoing').append(message + '<br />');
-  });
-}
