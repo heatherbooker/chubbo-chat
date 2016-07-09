694ad1fd3f42a31cf07fdc5088cfbee40750531f
Heather Booker <Heather Booker>
2016-07-08 22:57:36 -0700

    go back to small space at top between lines

--- public/javascripts/components/dashboard.js
+++ public/javascripts/components/dashboard.js
@@ -2,7 +2,6 @@ var dashboard = Vue.extend({
   template: `
   <div class="container-fluid">
     <div class="row">
-    hello world, this is <strong>dashboard</strong>
     <left-panel></left-panel>
     </div>
   </div>
