6ed2a185412e5f8fad1edacc91cb8eae61effe13
Heather Booker <Heather Booker>
2016-07-11 16:09:03 -0700

    should show logout as part of menu on mobile

--- public/javascripts/components/leftPanel.js
+++ public/javascripts/components/leftPanel.js
@@ -4,19 +4,25 @@ var leftPanel = Vue.extend({
     <div class="cc-leftPanel">
       <img v-bind:src=imgSrc class="cc-userIcon-leftPanel"/>
       <p class="cc-userEmail-leftPanel"> {{ userInfo.email }} </p>
+      <p v-show="onMobile"> logout </p>
     </div>
   </div>
   `,
   data: function() {
-    var userInfo = firebase.auth().currentUser;
+    var userInfo = firebase.auth().currentUser,
+        onMobile = false;
     //default user icon
     var imgSrc = 'https://s.ytimg.com/yts/img/avatar_720-vflYJnzBZ.png';
     if (userInfo.photoURL) {
       imgSrc = userInfo.photoURL;
     }
+    if ($(window).width() <= 400) {
+      onMobile = true;
+    }
     return {
       userInfo,
-      imgSrc
+      imgSrc,
+      onMobile
     };
   },
 });
