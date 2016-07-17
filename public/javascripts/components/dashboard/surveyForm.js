d61a2782d3589f238382679cc5503a58bbbeca62
Heather Booker <Heather Booker>
2016-07-17 01:24:13 -0700

    add publish button but can't center :(

--- public/javascripts/components/dashboard/surveyForm.js
+++ public/javascripts/components/dashboard/surveyForm.js
@@ -2,28 +2,29 @@ window.ChubboChat.components.surveyForm = Vue.extend({
   template: `
     <div class="col-md-9 col-xs-12">
       <div class="row">
-        <div class="col-md-8 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12">
-          <div class="cc-surveyForm">
+        <div class="col-md-11 col-md-offset-1">
+          <div class="cc-surveyFormPage">
             <input
               type="text"
               v-model="title"
               class="cc-titleInput"
               placeholder="Title..."
             >
-            <ol class="cc-surveyFormList">
-              <li><input type="text" v-model="question1" class="cc-questionInput"></li>
-              <li><input type="text" v-model="question2" class="cc-questionInput"></li>
-              <li><input type="text" v-model="question3" class="cc-questionInput"></li>
-              <li><input type="text" v-model="question4" class="cc-questionInput"></li>
-              <li><input type="text" v-model="question5" class="cc-questionInput"></li>
-            </ol>
+            <div class="cc-surveyFormList">
+              <div><input type="text" v-model="question1" class="cc-questionInput"></div>
+              <div><input type="text" v-model="question2" class="cc-questionInput"></div>
+              <div><input type="text" v-model="question3" class="cc-questionInput"></div>
+              <div><input type="text" v-model="question4" class="cc-questionInput"></div>
+              <div>
+                <input type="text" v-model="question5" class="cc-questionInput cc-lastQuestion">
+                <span class="cc-submitSurveyFormBtn">
+                  Publish
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  `,
-  data: function() {
-    return {
-    };
-  }
+  `
 });
