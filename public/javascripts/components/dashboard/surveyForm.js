f89851609fa82c00b8fc32a906dc8d5b0ef82022
Heather Booker <Heather Booker>
2016-07-16 22:40:26 -0700

    make position absolute

--- public/javascripts/components/dashboard/surveyForm.js
+++ public/javascripts/components/dashboard/surveyForm.js
@@ -3,19 +3,21 @@ window.ChubboChat.components.surveyForm = Vue.extend({
     <div class="col-md-9">
       <div class="row">
         <div class="col-md-8 col-md-offset-1">
-          <input
-            type="text"
-            v-model="title"
-            class="cc-titleInput"
-            placeholder="Title..."
-          >
-          <ol class="cc-surveyFormList">
-            <li><input type="text" v-model="question1" class="cc-questionInput"></li>
-            <li><input type="text" v-model="question2" class="cc-questionInput"></li>
-            <li><input type="text" v-model="question3" class="cc-questionInput"></li>
-            <li><input type="text" v-model="question4" class="cc-questionInput"></li>
-            <li><input type="text" v-model="question5" class="cc-questionInput"></li>
-          </ol>
+          <div class="cc-surveyForm">
+            <input
+              type="text"
+              v-model="title"
+              class="cc-titleInput"
+              placeholder="Title..."
+            >
+            <ol class="cc-surveyFormList">
+              <li><input type="text" v-model="question1" class="cc-questionInput"></li>
+              <li><input type="text" v-model="question2" class="cc-questionInput"></li>
+              <li><input type="text" v-model="question3" class="cc-questionInput"></li>
+              <li><input type="text" v-model="question4" class="cc-questionInput"></li>
+              <li><input type="text" v-model="question5" class="cc-questionInput"></li>
+            </ol>
+          </div>
         </div>
       </div>
     </div>
