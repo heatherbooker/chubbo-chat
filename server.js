68524122f4a8b255271fc6f7d2983ffa8660dd46
Heather Booker <Heather Booker>
2016-07-18 12:21:33 -0700

    add autofixer for css

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var autoprefixer = require('express-autoprefixer');
 
 
 var server = express();
@@ -9,6 +10,12 @@ var port = process.env.PORT || 3000;
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'pug');
 server.use(express.static(path.join(__dirname, 'public')));
+
+//add vendor prefixes to css
+server.use(autoprefixer({
+  browsers: 'last 2 versions',
+  remove: false
+}));
 
 server.get('/', function(req, res, next) {
   res.render('index');
