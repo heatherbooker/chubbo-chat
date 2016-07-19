cc1da1984975ce7161470646aeb3ade9bd5d911a
Heather Booker <Heather Booker>
2016-07-19 10:46:35 -0700

    add inputs dynamically

--- public/javascripts/components/dashboard/surveyForm.js
+++ public/javascripts/components/dashboard/surveyForm.js
@@ -2,7 +2,7 @@ window.ChubboChat.components.surveyForm = Vue.extend({
   template: `
     <div class="cc-surveyFormPage">
       <div class="cc-surveyFormInputs">
-        <div class="cc-inputRow">
+        <div class="cc-titleInputRow">
           <input
             type="text"
             v-model="title"
@@ -10,32 +10,45 @@ window.ChubboChat.components.surveyForm = Vue.extend({
             placeholder="Title..."
           >
         </div>
-        <div class="cc-inputRow">
-          <span class="cc-questionNumber">1.</span>
-          <input type="text" v-model="question1" class="cc-questionInput">
+        <div class="cc-questionInputRow" v-for="question in questions">
+          <input
+            type="text"
+            v-model=question
+            class="cc-questionInput"
+          >
         </div>
-        <div class="cc-inputRow">
-          <span class="cc-questionNumber">2.</span>
-          <input type="text" v-model="question2" class="cc-questionInput">
-        </div>
-        <div class="cc-inputRow">
-          <span class="cc-questionNumber">3.</span>
-          <input type="text" v-model="question3" class="cc-questionInput">
-        </div>
-        <div class="cc-inputRow">
-          <span class="cc-questionNumber">4.</span>
-          <input type="text" v-model="question4" class="cc-questionInput">
-        </div>
-        <div class="cc-inputRow">
-          <span class="cc-questionNumber">5.</span>
-          <input type="text" v-model="question5" class="cc-questionInput">
+        <div class="cc-questionInputRow">
+          <input
+            type="text"
+            v-model="newQuestion"
+            class="cc-questionInput"
+            v-on:keyup.enter="addNewQuestion"
+          >
         </div>
       </div>
       <div class="cc-submitBtnContainer">
-        <span class="cc-submitSurveyFormBtn">
+        <span
+          class="cc-submitSurveyFormBtn"
+          v-on:click="handlePublishBtn"
+        >
           Publish
         </span>
       </div>
     </div>
-  `
+  `,
+  data: function() {
+    return {
+      title: this.title,
+      questions: []
+    };
+  },
+  methods: {
+    addNewQuestion: function () {
+      this.questions.push(this.newQuestion);
+      this.newQuestion = '';
+    }
+    handlePublishBtn: function() {
+      this.questions.push(this.newQuestion);
+    }
+  }
 });
