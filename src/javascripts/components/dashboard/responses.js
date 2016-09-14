7f2cfede8bdafdfeffbc47d505fe4a3ee30a42b9
Heather Booker <Heather Booker>
2016-09-13 17:03:05 -0700

    Revert "responses are always undefined...why..why"
    This reverts commit 9c350cdace9dbe1b4a7036ca141a65064df5c1c0.

--- src/javascripts/components/dashboard/responses.js
+++ src/javascripts/components/dashboard/responses.js
@@ -4,8 +4,6 @@ import Vue from 'vue'
 import store from '../../store.js'
 //services
 import surveyApi from '../../services/surveyApi.js'
-// Components
-import responseBlock from './responseBlock.js';
 //styles
 import '../../../stylesheets/responses.css'
 
@@ -27,12 +25,13 @@ export default Vue.extend({
             .then((questions) => {
               this.questions = questions;
               transition.next();
+              this.setResponses(this.survey);
             });
       }
     }
   },
   template: `
-    <div class="cc-responsesPage" v-if="!$loadingRouteData">
+    <div class="cc-responsesPage">
       <div class="cc-responsesPage-container">
         <div
           v-for="question in questions"
@@ -40,19 +39,20 @@ export default Vue.extend({
           v-if="survey.isPublished"
         >
           <div class="cc-responsesPage-questionRow" @click="toggleViewReponses(question)">
-            <p class="cc-responsesPage-question">
-              <img
-                :src="arrowImgSrc"
-                :class="question.revealResponses ? arrowClassReveal : arrowClass"
-              />
-              {{ question.text }}
-            </p>
+            <img
+              :src="arrowImgSrc"
+              :class="question.revealResponses ? arrowClassReveal : arrowClass"
+            />
+            <p class="cc-responsesPage-question">{{ question.text }}</p>
           </div>
-          <response-block
-            :survey="survey"
-            :question="question"
-            :question-index="$index"
-          ></response-block>
+          <p
+            v-for="response in question.responses"
+            track-by="$index"
+            v-show="question.revealResponses"
+            class="cc-responsesPage-response"
+          >
+            {{ response }}
+          </p>
         </div>
         <p v-if="!survey.isPublished" class="cc-responsesPage-errorNotice">
           Publish your current survey or <br> select a published survey to see responses!
@@ -60,9 +60,6 @@ export default Vue.extend({
       </div>
     </div>
   `,
-  components: {
-    'response-block': responseBlock
-  },
   data: function() {
     return {
       questions: [],
@@ -76,13 +73,23 @@ export default Vue.extend({
       var promise = new Promise((resolve, reject) => {
         resolve(survey.questions.map((question) => {
           return {
-            text: question.text,
+            text: question,
             responses: [],
             revealResponses: false
           };
         }));
       });
       return promise;
+    },
+    setResponses: function(survey) {
+      if ('responses' in survey) {
+        var numOfQuestions = this.questions.length;
+        for (var responseKey in survey.responses) {
+          for (var i = 0; i < numOfQuestions; i ++) {
+            this.questions[i].responses.push(survey.responses[responseKey][i].text)
+          }
+        }
+      }
     },
     toggleViewReponses: function(question) {
       if (question.responses.length < 1) {
